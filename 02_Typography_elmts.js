8d7f369f982a8b0f9db83cd03b566cf9dce88d40
Ayaz Shaik <148182687+shaikayaz9@users.noreply.github.com>
2025-02-24 15:06:13 +0530

    Create 02_Typography_elmts.js

---
+++ 02_Typography_elmts.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+function Login() {
+  return (
+    <>
+      <div>hi</div>
+      <Typography
+        variant="h6"
+        color="danger"
+        gutterBottom
+        noWrap
+        align="center"
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum numquam est
+        praesentium perferendis harum ex qui recusandae? Vitae, sed! Cum odio
+        fuga rem asperiores accusantium, commodi recusandae expedita laboriosam
+        illum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
+        numquam est praesentium perferendis harum ex qui recusandae? Vitae, sed!
+        Cum odio fuga rem asperiores accusantium, commodi recusandae expedita
+        laboriosam illum! Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Eum numquam est praesentium perferendis harum ex qui recusandae?
+        Vitae, sed! Cum odio fuga rem asperiores accusantium, commodi recusandae
+        expedita laboriosam illum!
+      </Typography>
+
+      <Typography variant="h4" color="primary">
+        ayaz shaik
+      </Typography>
+      <Typography variant="h4" color="secondary">
+        ayaz shaik
+      </Typography>
+      <Typography variant="h4" color="error">
+        ayaz shaik
+      </Typography>
+      <Typography variant="h4" color="success">
+        ayaz shaik
+      </Typography>
+      <Typography variant="h4" color="textDisabled">
+        ayaz shaik
+      </Typography>
+      <Typography variant="h4" color="string">
+        ayaz shaik
+      </Typography>
+      <Typography variant="h4" color="textPrimary">
+        ayaz shaik
+      </Typography>
+      <Typography variant="h4" color="warning">
+        ayaz shaik
+      </Typography>
+      <Typography variant="h4" color="info">
+        ayaz shaik
+      </Typography>
+      <Typography variant="h4" color="textSecondary">
+        ayaz shaik
+      </Typography>
+    </>
+  );
+}
+
+export default Login;
